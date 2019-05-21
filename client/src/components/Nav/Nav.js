84620ade8f3211ea8a12b7a396cdde6073f3fa8d
Ashley <ashleymwilloughby@gmail.com>
2019-05-20 20:22:42 -0500

    added modals to the nav bar and added search bar to page

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,18 +1,98 @@
-import React from "react";
+import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import "./Nav.css";
 
-function Nav(props) {
-    return (
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <a className="navbar-brand" href="/api/logout">
-                    Logout
-                    </a>
-                </div>
-            </div>
-        </nav>
-    );
-};
+class Header extends Component {
+  state = {
+    show: false,
+    type: "", // "login" displays the login modal, "signup" displays the sign up modal
 
-export default Nav;
+    // Form inputs
+    email: "",
+    password: "",
+    confirm: ""
+  };
+
+  handleInputChange = event => {
+      const { name, value } = event.target;
+      this.setState({ [name]: value }, () => console.log(this.state[name]));
+  };
+
+  handleClose = () => {
+    this.setState({ show: false });
+  };
+
+  handleOpen = type => {
+    this.setState({
+      show: true,
+      type: type
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.type === "Sign Up") {
+        if (this.state.password !== this.state.confirm) {
+            console.log("Passwords do not match")
+            this.setState({
+                password: "",
+                confirm: ""
+            })
+        }
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar fixed="top" bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link onClick={() => this.handleOpen("Login")}>Login</Nav.Link>
+            <Nav.Link onClick={() => this.handleOpen("Sign Up")}>
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.state.type}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.handleFormSubmit}>
+              <Form.Group controlId="formGroupEmail">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control name = "email" value={this.state.email} onChange={this.handleInputChange} type="email" placeholder="Enter Email" />
+              </Form.Group>
+              <Form.Group controlId="formGroupPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name="password" value={this.state.password} onChange={this.handleInputChange} type="password" placeholder="Password" />
+              </Form.Group>
+              {this.state.type === "Sign Up" && (
+                <Form.Group controlId="formGroupConfirm">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control name="confirm" value={this.state.confirm} onChange={this.handleInputChange} type="password" placeholder="Confirm Password" />
+                </Form.Group>
+              )}
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleFormSubmit}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,65 +1,25 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import Row from "react-bootstrap/Row";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 import API from "../../utils/API.js";
 import "./Home.css";
 
 class Home extends Component {
-    state = {
-        email: "",
-        password: "",
-        isLoggedIn: false
-    };
-
-    handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        }, () => console.log(this.state));
-    }
-
-    handleFormSubmit = event => {
-        event.preventDefault();
-        API.userSignup(this.state).then(response => {
-            console.log(response.data);
-            this.setState({ isLoggedIn: true });
-        }).catch(err => {
-            console.log(err);
-        })
-    };
-
     render() {
-
-        if(this.state.isLoggedIn) {
-            return <Redirect to="/members" />
-        }
-
         return (
-            <Row>
-                <div className="col-md-6 col-md-offset-3">
-                    <h2>Sign Up Form</h2>
-                    <form className="signup">
-                    <div className="form-group">
-                        <label htmlFor="email-input">Email address</label>
-                        <input onChange={this.handleInputChange} name="email" value={this.state.email} className="form-control" id="email-input" placeholder="Email" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password-input">Password</label>
-                        <input onChange={this.handleInputChange} name="password" value={this.state.password} className="form-control" id="password-input" placeholder="Password" />
-                    </div>
-                    {/* <div style="display: none" id="alert" className="alert alert-danger" role="alert">
-                        <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                        <span className="sr-only">Error:</span> <span className="msg"></span>
-                    </div> */}
-                    <button onClick={this.handleFormSubmit} type="submit" className="btn btn-default">Sign Up</button>
-                    </form>
-                    <br />
-                    <p>Or log in <a href="/login">here</a></p>
-                </div>
-            </Row>
-            
-        );
-    };
-};
+            <InputGroup className="mb-3">
+            <FormControl
+              placeholder="Start typing here to search the spell list..."
+              aria-label="Spell search"
+              aria-describedby="basic-addon2"
+            />
+            <InputGroup.Append>
+              <Button variant="outline-secondary">Search</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        )
+    }
+}
 
 export default Home;
