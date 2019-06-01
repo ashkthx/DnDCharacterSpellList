dc61f13764eb915dff2cd020cf7bb351b6500c03
Ashley <ashleymwilloughby@gmail.com>
2019-06-01 08:33:17 -0500

    autocomplete styling

--- client/src/components/FormComplete/FormComplete.js
+++ client/src/components/FormComplete/FormComplete.js
@@ -12,10 +12,10 @@ const styles = {
       boxShadow: "0 2px 12px rgba(0, 0, 0, 0.1)",
       background: "rgba(255, 255, 255, 0.9)",
       padding: "2px 0",
-      fontSize: "90%",
+      fontSize: "100%",
       position: "fixed",
       overflow: "auto",
-      maxHeight: "15%",
+      maxHeight: "25%",
       zIndex: 100,
       paddingLeft: "13px"
   },
